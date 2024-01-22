d15a0eea8be192ed820737bcde2abb0a93224247
esrakonya <esrakonya6@gmail.com>
2024-01-23 01:21:12 +0300

    second upgrade

--- app/src/main/java/com/example/finalproject/ui/AddPhotoFragment.java
+++ app/src/main/java/com/example/finalproject/ui/AddPhotoFragment.java
@@ -1,21 +1,29 @@
 package com.example.finalproject.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.finalproject.R;
 import com.example.finalproject.databinding.FragmentAddphotoBinding;
 import com.example.finalproject.models.AddPhotoViewModel;
 
 public class AddPhotoFragment extends Fragment {
 
     private FragmentAddphotoBinding binding;
+
+    Button btnSec, btnPaylas;
+
+    private int images = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,14 +33,19 @@ public class AddPhotoFragment extends Fragment {
         binding = FragmentAddphotoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        addPhotoViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        btnSec = root.findViewById(R.id.btnSec);
+        btnPaylas = root.findViewById(R.id.btnPaylas);
+
+        btnSec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            }
+        });
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+
+
 }
